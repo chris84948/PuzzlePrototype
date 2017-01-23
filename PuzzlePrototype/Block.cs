e2128d044850375811ff023e4cd027410f30fcf4
chris84948 <chris84948@gmail.com>
2017-01-22 21:42:36 -0600

    Initial check-in

---
+++ PuzzlePrototype/Block.cs
@@ -0,0 +1,103 @@
+﻿using JustMVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PuzzlePrototype
+{
+    public class Block : MVVMBase
+    {
+        public static List<Color> ColorList = new List<Color>()
+        {
+            (Color)ColorConverter.ConvertFromString("#860DFF"),
+            (Color)ColorConverter.ConvertFromString("#3D0CE8"),
+            (Color)ColorConverter.ConvertFromString("#000CFF"),
+            (Color)ColorConverter.ConvertFromString("#0C50E8"),
+            (Color)ColorConverter.ConvertFromString("#0D97FF")
+        };
+
+        public int BlockSize { get; set; } = MainWindowViewModel.BLOCK;
+
+        private double _bottom;
+        public double Bottom
+        {
+            get { return _bottom; }
+            set
+            {
+                _bottom = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _left;
+        public double Left
+        {
+            get { return _left; }
+            set
+            {
+                _left = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _x;
+        public int X
+        {
+            get { return _x; }
+            set
+            {
+                _x = value;
+                Left = _x * MainWindowViewModel.BLOCK;
+            }
+        }
+
+        private int _y;
+        public int Y
+        {
+            get { return _y; }
+            set
+            {
+                _y = value;
+                Bottom = _y * MainWindowViewModel.BLOCK;
+            }
+        }
+
+        private Color _color;
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private static Random _random = new Random(DateTime.Now.Millisecond);
+
+        public Block(int x, int y)
+            : this(x, y, ColorList[_random.Next(ColorList.Count)])
+        { }
+
+        public Block(int x, int y, Color color)
+        {
+            X = x;
+            Y = y;
+
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"({ X }, {Y}), { Color.ToString() }";
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ PuzzlePrototype/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using JustMVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlePrototype
+{
+    public class MainWindowViewModel : MVVMBase
+    {
+        public const int NUM_COLS = 10;
+        public const int NUM_ROWS = 16;
+        public const int BLOCK = 40;
+
+        private ObservableCollection<Block> _blocks;
+        public ObservableCollection<Block> Blocks
+        {
+            get { return _blocks; }
+            set
+            {
+                _blocks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Width { get { return NUM_COLS * BLOCK; } }
+        public double Height { get { return NUM_ROWS * BLOCK; } }
+
+        public MainWindowViewModel()
+        {
+            Blocks = new ObservableCollection<Block>();
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    for (int row = 0; row < NUM_ROWS; row++)
+                    {
+                        for (int col = 0; col < NUM_COLS; col++)
+                        {
+                            var newBlock = new Block(0, NUM_ROWS - 1);
+
+                            App.Current.Dispatcher.Invoke(() => Blocks.Add(newBlock));
+                            await Task.Delay(400);
+                            App.Current.Dispatcher.Invoke(() => { newBlock.X = col; newBlock.Y = row; });
+                            await Task.Delay(200);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show(ex.ToString());
+                }
+            });
+        }
+    }
+}
